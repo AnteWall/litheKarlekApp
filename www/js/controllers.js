7a1557a8e04e1b2b5cfc9338e2758496f8968ba6
Ante Wall <antewall@gmail.com>
2015-02-19 09:47:02 +0100

    Added Image Slider on Profile

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -47,15 +47,21 @@ angular.module('starter.controllers', [])
   }
 })
 
-.controller('ProfileCtrl', function($scope,apiFactory) {
+.controller('ProfileCtrl', function($scope,apiFactory,$ionicLoading) {
   $scope.profile;
-
+  $scope.images = ["https://fbcdn-sphotos-d-a.akamaihd.net/hphotos-ak-xap1/v/t1.0-9/1483030_10204500224600786_5797545058229427005_n.jpg?oh=c859bf8a21ecb7d9d8e8d9f8b0d9e466&oe=558A67E9&__gda__=1431226320_6b1864eafca1e387bd7044aa27db77a9","https://scontent-ams.xx.fbcdn.net/hphotos-xpa1/v/t1.0-9/10334314_10152689726059713_1090164303467743202_n.jpg?oh=875a92fec7accaeebc26c807db612c7f&oe=558A496F","https://fbcdn-sphotos-b-a.akamaihd.net/hphotos-ak-xpf1/v/t1.0-9/1150348_745846915463162_5547011624121043311_n.jpg?oh=9c9d6541f428d0200b9d73601534d10e&oe=554F0433&__gda__=1430948594_fdd64140d0d6e3d1521c39cc99517ac0"]
   getProfile();
 
   function getProfile(){
+    $ionicLoading.show({
+     template: 'Laddar...' 
+    });
+ 
     apiFactory.getProfile().success(function(data){
       $scope.profile = data;
+      $ionicLoading.hide();
     }).error(function(error){
+      $ionicLoading.hide();
     });
   }
 })
